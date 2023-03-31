b11d7161809adbb01f9f0d1469e0ceb5d69bb4af
Purushotham <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-03-31 16:00:28 +0530

    Updated scriptPs files

--- ScriptsPS/ReplaceFileContentAppsettingsjson.ps1
+++ ScriptsPS/ReplaceFileContentAppsettingsjson.ps1
@@ -1,5 +1,5 @@
 # Get the content of file B
-$BContent = Get-Content -Path "D:\a\1\s\appsettingsjson.txt" -Raw
+$BContent = Get-Content -Path "D:\a\1\s\ScriptsPS\appsettingsjson.txt" -Raw
 
 # Set the content of file A to be the same as file B
 Set-Content -Path "D:\a\1\s\EshopApplication\appsettings.json" -Value $BContent
