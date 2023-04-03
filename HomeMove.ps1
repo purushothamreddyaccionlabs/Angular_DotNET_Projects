a393c34a23ac44ddfbd645659f82e8a6de760de0
Purushotham <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-04-03 08:22:24 +0530

    Add files via upload

---
+++ HomeMove.ps1
@@ -0,0 +1,9 @@
+# Define the paths for folders A and B
+$folderA = "D:\a\1\s\Angular_DotNET_Projects"
+$folderB = "D:\a\1\s"
+
+# Create folder A inside folder B
+New-Item -ItemType Directory -Path $folderA
+
+# Move all files and folders from folder B to folder A
+Get-ChildItem $folderB | Move-Item -Destination $folderA
