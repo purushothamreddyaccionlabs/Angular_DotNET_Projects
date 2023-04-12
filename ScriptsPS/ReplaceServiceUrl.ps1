4f5b7af03ab3e80b56d598c740d94a6e2b4189e3
Purushotham <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-04-12 22:05:51 +0530

    Update ReplaceServiceUrl.ps1

--- ScriptsPS/ReplaceServiceUrl.ps1
+++ ScriptsPS/ReplaceServiceUrl.ps1
@@ -1,3 +1,3 @@
 $content = Get-Content -Path D:\a\1\s\src\app\service-API\api-services.service.ts
-$content = $content -replace 'https://localhost:7103', 'https://dev-eshopcommerce.azurewebsites.net'
+$content = $content -replace 'https://localhost:7103', 'https://backend-eshopcommerce.azurewebsites.net'
 Set-Content -Path D:\a\1\s\src\app\service-API\api-services.service.ts -Value $content
